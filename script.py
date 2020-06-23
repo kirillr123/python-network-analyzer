be822dff60db0106c9ce5b2f4eb8de22e8de31ed
Kirill <kradzanovskij@gmail.com>
2020-06-23 14:05:52 +0500

    Refactor bad parsing

--- script.py
+++ script.py
@@ -1,38 +1,25 @@
 import scapy.all
 from clickhouse_driver import Client
+from scapy2dict import to_dict
 
-def expand(x):
-    yield x
-    while x.payload:
-        x = x.payload
-        yield x
-
-
-#Called each time a packet is caught
+#metrics
+#unique ips
+#action is called each time a packet is caught
 def custom_action(packet):
-    data = {'InterfaceLayer': None, 'InternetLayer': None, 'TransportLayer': None, 'ApplicationLayer': None}
-    plist = list(expand(packet))
-    plist.reverse()
-    for k, v in data.items():
-        if not plist:
-            break
-        layer = plist.pop()
-        field_names = [field.name for field in layer.fields_desc]
-        #remove raw data
-        if layer.name == "Raw":
-            data[k] = layer.name
-        else:
-            data[k] = layer.name + str({field_name: getattr(packet, field_name) for field_name in field_names})
-    print(data)
-    if data["ApplicationLayer"]:
-        print(client.execute(f'INSERT INTO test.mytable VALUES',
-            [data] ))
-    input()
-    return None
+    data = {'InterfaceLayer': "None", 'InternetLayer': "None", 'TransportLayer': "None", 'ApplicationLayer': "None"}
+    d = to_dict(packet, strict=False)
+    #dunno why but need to iterate over d.maps which is a [], otherwise the order is fucked up
 
-## Setup sniff, filtering for IP traffic
+    for layer, key in zip(d.maps, data):
+        x = next(iter(layer.items())) #tuple
+        data[key] = str(x[0]) + " " + str(x[1])
+    print(client.execute(f'INSERT INTO test.mytable VALUES',
+             [data] ))
+    print(f"Packet sent...\n{data}")
+    return ""
 
-client = Client(host='localhost', password='admin')
+
+client = Client(host='localhost')
 #create structure
 client.execute("CREATE DATABASE IF NOT EXISTS test")
 client.execute("""CREATE TABLE IF NOT EXISTS test.mytable
@@ -43,12 +30,10 @@ client.execute("""CREATE TABLE IF NOT EXISTS test.mytable
     ApplicationLayer String
 ) ENGINE = Memory
 """)
-#TinyLog engine for not testing
-#client.execute('INSERT INTO test VALUES (%(a)s), (%(b)s)',{'a': 1, 'b': 2})
+#TinyLog engine for not testing, right not data is in RAM
 
 print(client.execute('SHOW TABLES FROM test'))
-print(client.execute('SELECT * FROM test.mytable'))
-
-print("Db is set up, waiting for packets to arrive....\nPress q+Enter to exit\n")
+print("Db is set up, waiting for packets to arrive....\n")
 #filter incoming traffic maybe
-scapy.all.sniff(prn=custom_action, count=100000)
+# Setup sniff, filtering for incoming trafic
+scapy.all.sniff(filter=f"dst {scapy.all.get_if_addr(conf.iface)}", prn=custom_action, count=100000)
