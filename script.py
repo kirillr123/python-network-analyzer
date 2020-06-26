d91be21e63d03946efb31c7a6e204f87a911742c
Kirill <kradzanovskij@gmail.com>
2020-06-26 18:56:59 +0500

    Better metric collection

--- script.py
+++ script.py
@@ -3,6 +3,9 @@ from clickhouse_driver import Client
 from scapy2dict import to_dict
 import itertools
 from datetime import datetime
+from requests import get
+from kiss_headers import parse_it
+
 
 db_name = "nir"
 metrics_table = "metrics"
@@ -21,54 +24,58 @@ def startup():
     """)
     client.execute(f""" CREATE TABLE IF NOT EXISTS {db_name}.{metrics_table}
     (
-        StartTime String,
-        EndTime String,
-        UniqueIps String,
-        TotalPackets String
+        Time String,
+        Ip String,
+        UserAgent String
     ) ENGINE = Memory
     """)
     #TinyLog engine for not testing, right now data is in RAM
-    #print(client.execute(f'SHOW TABLES FROM {db_name}'))
     return client
 
 #action is called each time a packet is caught
 def custom_action(packet):
     data = {'InterfaceLayer': "None", 'InternetLayer': "None", 'TransportLayer': "None", 'ApplicationLayer': "None"}
+    
     d = to_dict(packet, strict=False)
     #dunno why but need to iterate over d.maps which is a [], otherwise the order is fucked up
+    ip = d["IP"]["src"]
+    time = datetime.now()
+    user_agent = None
     try:
-        if d["IP"]["src"] not in unique_ips:
-            unique_ips.append(d["IP"]["src"])
+        if ip not in unique_ips:
+            unique_ips.append(ip)
     except:
         None
+    
+    if "Raw" in d and d["Raw"]["load"].startswith(b"GET"):
+        try:
+            headers = parse_it(d["Raw"]["load"])
+            user_agent = headers.user_agent
+        except:
+            print("Exception!!!!!")
 
     for layer, key in zip(d.maps, data):
         x = next(iter(layer.items())) #tuple
         data[key] = str(x[0]) + " " + str(x[1])
+
     client.execute(f'INSERT INTO {db_name}.{data_table} VALUES',
              [data] )
-
-    print(f"Unique IP addresses: {len(unique_ips)}, packets: {next(counter) + 1}", end='\r')
+    if user_agent:
+        client.execute(f'INSERT INTO {db_name}.{metrics_table} VALUES',
+        [{"Time": str(time),"Ip": ip, "UserAgent": str(user_agent)}])
+    print(f"Unique IP addresses: {unique_ips}, packets: {next(counter) + 1}", end='\r')
     return None
 
 if __name__ == "__main__":
     client = startup()
     
     while True:
-        metrics = input("Do you want to collect metrics? If yes you must let program to finish (yes/no)....")
-        if metrics == "yes":
-            start_time = datetime.now()
         counter = itertools.count()
         unique_ips=[]
 
-        scapy.all.sniff(filter=f"dst {scapy.all.get_if_addr(conf.iface)}", prn=custom_action, count=int(input("Enter how much packets do you want to analyze \n")))
+        scapy.all.sniff(filter=f"src 192.168.0.100", prn=custom_action, count=int(input("Enter how much packets do you want to analyze \n")))
         print()
         print("Db is set up, waiting for packets to arrive....\n")
-
-        if metrics == "yes":
-            end_time = datetime.now()
-            client.execute(f"INSERT INTO {db_name}.{metrics_table} VALUES",
-            [{"StartTime": str(start_time),"EndTime": str(end_time), "UniqueIps": str(len(unique_ips)), "TotalPackets": str(next(counter))}])
 
         if input("Enter 1 to finish..\n") == "1":
             break
